3879a829148257be0859055c32fdcfd0be161a3b
dstepp <dstepp@petsafe.net>
2017-07-07 15:52:09 -0400

    Fixed the filename casing.

--- gatsby-site/src/layouts/index.js
+++ gatsby-site/src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import Link from "gatsby-link"
 import Helmet from "react-helmet"
 import {rhythm} from "../utils/typography"
-import Header from "../components/common/header"
+import Header from "../components/common/Header"
 import Footer from "../components/common/Footer";
 
 
