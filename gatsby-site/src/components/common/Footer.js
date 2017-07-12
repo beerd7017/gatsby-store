d93f19cff1dfc0746045805f9c8133689bc335a6
dstepp <dstepp@petsafe.net>
2017-07-12 11:37:58 -0400

    Added constructor to the footer.

--- gatsby-site/src/components/common/Footer.js
+++ gatsby-site/src/components/common/Footer.js
@@ -1,6 +1,10 @@
 import React from "react"
 
 class Footer extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
     render() {
         return (
             <footer id="footer">
