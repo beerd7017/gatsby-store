9f6930f7ddb890ce81b856052004a9fbcc6ed39b
dstepp <dstepp@petsafe.net>
2017-07-12 13:35:50 -0400

    Added content to the ProductList.

--- gatsby-site/src/components/product/ProductList.js
+++ gatsby-site/src/components/product/ProductList.js
@@ -8,7 +8,154 @@ class ProductList extends React.Component {
 
     render() {
         return (
-            <p>Hi!</p>
+            <div className="col-sm-9">
+                <p className="text-muted lead">Fountains keep your pet healthy and hydrated with fresh, filtered,
+                    circulating water.</p>
+                <div className="row products">
+                    <div className="col-md-4 col-sm-6">
+                        <div className="product">
+                            <div className="image">
+                                <img src="http://placehold.it/500x500"/>
+                            </div>
+                            <div className="text">
+                                <h3><a href="#">Drinkwell Fountain</a></h3>
+                                <p className="text-muted">#PWWW-324342</p>
+                                <p className="price">53.45</p>
+                                <a href="#" className="btn btn-template-main"><i className="fa fa-shopping-cart"/>Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+
+
+
+
+                    <div className="col-md-4 col-sm-6">
+                        <div className="product">
+                            <div className="image">
+                                <img src="http://placehold.it/500x500"/>
+                            </div>
+                            <div className="text">
+                                <h3><a href="#">Drinkwell Fountain</a></h3>
+                                <p className="text-muted">#PWWW-324342</p>
+                                <p className="price">53.45</p>
+                                <a href="#" className="btn btn-template-main"><i className="fa fa-shopping-cart"/>Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4 col-sm-6">
+                        <div className="product">
+                            <div className="image">
+                                <img src="http://placehold.it/500x500"/>
+                            </div>
+                            <div className="text">
+                                <h3><a href="#">Drinkwell Fountain</a></h3>
+                                <p className="text-muted">#PWWW-324342</p>
+                                <p className="price">53.45</p>
+                                <a href="#" className="btn btn-template-main"><i className="fa fa-shopping-cart"/>Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6">
+                        <div className="product">
+                            <div className="image">
+                                <img src="http://placehold.it/500x500"/>
+                            </div>
+                            <div className="text">
+                                <h3><a href="#">Drinkwell Fountain</a></h3>
+                                <p className="text-muted">#PWWW-324342</p>
+                                <p className="price">53.45</p>
+                                <a href="#" className="btn btn-template-main"><i className="fa fa-shopping-cart"/>Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6">
+                        <div className="product">
+                            <div className="image">
+                                <img src="http://placehold.it/500x500"/>
+                            </div>
+                            <div className="text">
+                                <h3><a href="#">Drinkwell Fountain</a></h3>
+                                <p className="text-muted">#PWWW-324342</p>
+                                <p className="price">53.45</p>
+                                <a href="#" className="btn btn-template-main"><i className="fa fa-shopping-cart"/>Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6">
+                        <div className="product">
+                            <div className="image">
+                                <img src="http://placehold.it/500x500"/>
+                            </div>
+                            <div className="text">
+                                <h3><a href="#">Drinkwell Fountain</a></h3>
+                                <p className="text-muted">#PWWW-324342</p>
+                                <p className="price">53.45</p>
+                                <a href="#" className="btn btn-template-main"><i className="fa fa-shopping-cart"/>Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6">
+                        <div className="product">
+                            <div className="image">
+                                <img src="http://placehold.it/500x500"/>
+                            </div>
+                            <div className="text">
+                                <h3><a href="#">Drinkwell Fountain</a></h3>
+                                <p className="text-muted">#PWWW-324342</p>
+                                <p className="price">53.45</p>
+                                <a href="#" className="btn btn-template-main"><i className="fa fa-shopping-cart"/>Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6">
+                        <div className="product">
+                            <div className="image">
+                                <img src="http://placehold.it/500x500"/>
+                            </div>
+                            <div className="text">
+                                <h3><a href="#">Drinkwell Fountain</a></h3>
+                                <p className="text-muted">#PWWW-324342</p>
+                                <p className="price">53.45</p>
+                                <a href="#" className="btn btn-template-main"><i className="fa fa-shopping-cart"/>Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6">
+                        <div className="product">
+                            <div className="image">
+                                <img src="http://placehold.it/500x500"/>
+                            </div>
+                            <div className="text">
+                                <h3><a href="#">Drinkwell Fountain</a></h3>
+                                <p className="text-muted">#PWWW-324342</p>
+                                <p className="price">53.45</p>
+                                <a href="#" className="btn btn-template-main"><i className="fa fa-shopping-cart"/>Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6">
+                        <div className="product">
+                            <div className="image">
+                                <img src="http://placehold.it/500x500"/>
+                            </div>
+                            <div className="text">
+                                <h3><a href="#">Drinkwell Fountain</a></h3>
+                                <p className="text-muted">#PWWW-324342</p>
+                                <p className="price">53.45</p>
+                                <a href="#" className="btn btn-template-main"><i className="fa fa-shopping-cart"/>Add to cart</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
