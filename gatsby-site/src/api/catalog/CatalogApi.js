0e570931e4287639cb9d0216c87a683819d7ad6f
dstepp <dstepp@petsafe.net>
2017-07-12 13:27:13 -0400

    Added a totalProduct property to the CatalogApi.

--- gatsby-site/src/api/catalog/CatalogApi.js
+++ gatsby-site/src/api/catalog/CatalogApi.js
@@ -3,7 +3,7 @@ class CatalogApi {
     static getCatalog() {
         var mockApiCall = new Promise((resolve, reject) => {
             setTimeout(function () {
-                resolve([{name: "Doors"}, {name: "Clothes"}, {name: "Feeders"}, {name: "Fountains"}, {name: "Harnesses/Leashes"}, {name: "Pet Tech"}, {name: "Toys"}, {name: "Training"}, {name: "Treats"}, {name: "Waste Management"}]);
+                resolve([{name: "Doors", totalProducts:"11" }, {name: "Clothes", totalProducts:"8"}, {name: "Feeders", totalProducts:"12"}, {name: "Fountains", totalProducts:"8"}, {name: "Harnesses/Leashes", totalProducts:"12"}, {name: "Pet Tech", totalProducts:"15"}, {name: "Toys", totalProducts:"23"}, {name: "Training", totalProducts:"13"}, {name: "Treats", totalProducts:"7"}, {name: "Waste Management", totalProducts:"14"}]);
             }, 1000);
         });
         return mockApiCall;
