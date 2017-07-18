a6a814621ea98835b3fbad95eec9031788f1edf7
dstepp <dstepp@petsafe.net>
2017-07-18 15:41:16 -0400

    Returning sub categories as objects.

--- gatsby-site/src/api/catalog/CatalogApi.js
+++ gatsby-site/src/api/catalog/CatalogApi.js
@@ -3,7 +3,106 @@ class CatalogApi {
     static getCatalog() {
         var mockApiCall = new Promise((resolve, reject) => {
             setTimeout(function () {
-                resolve([{name: "Doors", totalProducts:"11" }, {name: "Clothes", totalProducts:"8"}, {name: "Feeders", totalProducts:"12"}, {name: "Fountains", totalProducts:"8"}, {name: "Harnesses/Leashes", totalProducts:"12"}, {name: "Pet Tech", totalProducts:"15"}, {name: "Toys", totalProducts:"23"}, {name: "Training", totalProducts:"13"}, {name: "Treats", totalProducts:"7"}, {name: "Waste Management", totalProducts:"14"}]);
+                resolve([{
+                    name: "Doors",
+                    totalProducts: "11",
+                    childArray: [{
+                        subcatagory: [{key: "1", name: "Small Dog", url: "/"}]
+                    },
+                        {
+                            subcatagory: [{key: "1", name: "Big Dog", url: "/"}]
+                        }]
+                }, {
+                    name: "Clothes",
+                    totalProducts: "8",
+                    childArray: [{
+                        subcatagory: [{key: "2", name: "Small Dog", url: "/"}]
+                    },
+                        {
+                            subcatagory: [{key: "2", name: "Big Dog", url: "/"}]
+                        }]
+                },
+                    {
+                        name: "Feeders",
+                        totalProducts: "12",
+                        childArray: [{
+                            subcatagory: [{key: "3", name: "Small Dog", url: "/"}]
+                        },
+                            {
+                                subcatagory: [{key: "3", name: "Big Dog", url: "/"}]
+                            }]
+                    },
+                    {
+                        name: "Fountains",
+                        totalProducts: "8",
+                        childArray: [{
+                            subcatagory: [{key: "4", name: "Small Dog", url: "/"}]
+                        },
+                            {
+                                subcatagory: [{key: "4", name: "Big Dog", url: "/"}]
+                            }]
+                    },
+                    {
+                        name: "Harnesses/Leashes",
+                        totalProducts: "12",
+                        childArray: [{
+                            subcatagory: [{key: "5", name: "Small Dog", url: "/"}]
+                        },
+                            {
+                                subcatagory: [{key: "5", name: "Big Dog", url: "/"}]
+                            }]
+                    },
+                    {
+                        name: "Pet Tech",
+                        totalProducts: "15",
+                        childArray: [{
+                            subcatagory: [{key: "6", name: "Small Dog", url: "/"}]
+                        },
+                            {
+                                subcatagory: [{key: "6", name: "Big Dog", url: "/"}]
+                            }]
+                    },
+                    {
+                        name: "Toys",
+                        totalProducts: "23",
+                        childArray: [{
+                            subcatagory: [{key: "7", name: "Small Dog", url: "/"}]
+                        },
+                            {
+                                subcatagory: [{key: "7", name: "Big Dog", url: "/"}]
+                            }]
+                    },
+                    {
+                        name: "Training",
+                        totalProducts: "13",
+                        childArray: [{
+                            subcatagory: [{key: "8", name: "Small Dog", url: "/"}]
+                        },
+                            {
+                                subcatagory: [{key: "8", name: "Big Dog", url: "/"}]
+                            }]
+                    },
+                    {
+                        name: "Treats",
+                        totalProducts: "7",
+                        childArray: [{
+                            subcatagory: [{key: "9", name: "Small Dog", url: "/"}]
+                        },
+                            {
+                                subcatagory: [{key: "9", name: "Big Dog", url: "/"}]
+                            }]
+                    },
+                    {
+                        name: "Waste Management",
+                        totalProducts: "14",
+                        childArray: [{
+                            subcatagory: [{key: "10", name: "Small Dog", url: "/"}]
+                        },
+                            {
+                                subcatagory: [{key: "10", name: "Big Dog", url: "/"}]
+                            }]
+                    }
+                ]);
             }, 1000);
         });
         return mockApiCall;
--- gatsby-site/src/components/catalog/NavigationItem.js
+++ gatsby-site/src/components/catalog/NavigationItem.js
@@ -1,24 +1,52 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
+import {catalogActions} from "../../redux_modules/CatalogModule";
+
 
 class NavigationItem extends Component {
     constructor(props) {
         super(props);
+        this.displayName = this.displayName.bind(this);
     }
+    displayName(array) {
+        if (typeof(array.subcatagory) !== "undefined") {
+            //console.log("calling recursively");
+            return array.subcatagory.map(this.displayName);
 
+        } else {
+            //console.log("just returning some html");
+            return (<ul id="subcat" className="collapse in"><li><a href={array.url}>{array.name}</a></li></ul>);
+        }
+    }
     render() {
         let catalog = this.props.catalog;
+        console.log(catalog.childArray.map(this.displayName));
         return (
             <ul className="nav nav-pills nav-stacked category-menu" id="menu">
                 <li>
-                    <a href="#">{catalog.name}<span className="badge pull-right">{catalog.totalProducts}</span></a>
+                    <a href="#" data-toggle="collapse" data-target="#subcat">{catalog.name}<span
+                        className="badge pull-right">{catalog.totalProducts}</span></a>
                 </li>
-                <ul>
-                    <li><a href="#">Small Dog</a></li>
-                    <li><a href="#">Big Dog</a></li>
-                </ul>
+                    {
+                        catalog.childArray.map(this.displayName)
+                    }
             </ul>
         );
     }
 }
 
-export default NavigationItem;
+function mapStateToProps(state) {
+    return {
+        catalogs: state.catalogs
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        getCatalogs: () => {
+            dispatch(catalogActions.getCatalogs());
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavigationItem);
