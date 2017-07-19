a6c8e8812762d43765ba1f3ccda7ec8889eabd05
dstepp <dstepp@petsafe.net>
2017-07-19 09:37:33 -0400

    Created a new subSection class and api.

--- gatsby-site/src/store/configureStore.js
+++ gatsby-site/src/store/configureStore.js
@@ -1,15 +1,16 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux';
-import {catalogReducer} from "../redux_modules/CatalogModule";
-import {productReducer} from "../redux_modules/ProductModule";
 import {corporateReducer} from "../redux_modules/CorporateModule";
+import {catalogReducer} from "../redux_modules/CatalogModule";
+import {subSectionReducer} from "../redux_modules/SubSectionModule";
+import {productReducer} from "../redux_modules/ProductModule";
 import thunk from "redux-thunk";
-
 
 export default function configureStore(initialState) {
     const rootReducer = combineReducers({
+        corporate: corporateReducer,
         catalogs: catalogReducer,
-        products: productReducer,
-        corporate: corporateReducer
+        subSecitons: subSectionReducer,
+        products: productReducer
     });
     const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
     return store;
