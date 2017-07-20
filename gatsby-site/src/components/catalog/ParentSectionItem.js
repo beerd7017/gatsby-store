0dc4a502674519bbe5fd1002477b0c6177c0b3b4
dstepp <dstepp@petsafe.net>
2017-07-20 11:10:36 -0400

    Fixed sub category array processing.

--- gatsby-site/src/components/catalog/ParentSectionItem.js
+++ gatsby-site/src/components/catalog/ParentSectionItem.js
@@ -1,5 +1,10 @@
 import React, {Component} from "react";
 
+function myFunction(element) {
+
+    return <li><a href="/">{element}</a></li>;
+}
+
 class ParentSectionItem extends Component {
     constructor(props) {
         super(props);
@@ -7,7 +12,6 @@ class ParentSectionItem extends Component {
 
     render() {
         let catalog = this.props.catalog;
-        console.log(catalog.subSections);
 
         return (
             <ul className="nav nav-pills nav-stacked category-menu" id="menu">
@@ -15,14 +19,12 @@ class ParentSectionItem extends Component {
                     <a href="#" data-toggle="collapse" data-target="#Doors">{catalog.name}<span
                         className="badge pull-right">{catalog.totalProducts}</span></a>
                 </li>
-                <ul id="#" className="collapse in">
-
-                    <li><a href="/">{catalog.subSections}</a></li>
+                <ul id={catalog.name} className="collapse in">
+                    {catalog.subSections.map(myFunction)}
                 </ul>
             </ul>
         );
     }
 }
-
 
 export default ParentSectionItem;
